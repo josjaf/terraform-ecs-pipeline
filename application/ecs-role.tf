1c892d744f875918097af439ff065d925203d6ed
Josh Jaffe <josjaf@gmail.com>
2020-09-18 00:11:44 -0400

    ecs working

---
+++ application/ecs-role.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_role" "ecs-role" {
+  name_prefix = "${var.namespace}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy_attachment" "codepipeline-attach" {
+  role = aws_iam_role.ecs-role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+resource "aws_iam_role_policy_attachment" "codepipeline1" {
+  role = aws_iam_role.ecs-role.id
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "ecr"
+  role = aws_iam_role.ecs-role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
---
+++ application/ecs.tf
@@ -0,0 +1,145 @@
+data "aws_vpc" "VPC" {
+  filter {
+    name = "vpc-id"
+    values = [
+      var.vpc_id]
+  }
+}
+data "aws_subnet_ids" "public_subnets" {
+  vpc_id = var.vpc_id
+  filter {
+    name = "tag:Network"
+    values = [
+      "Public"]
+  }
+}
+
+### Security
+
+# ALB Security group
+# This is the group you need to edit if you want to restrict access to your application
+resource "aws_security_group" "lb" {
+  name        = "tf-ecs-alb"
+  description = "controls access to the ALB"
+  vpc_id      = data.aws_vpc.VPC.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Traffic to the ECS Cluster should only come from the ALB
+resource "aws_security_group" "ecs_tasks" {
+  name        = "tf-ecs-tasks"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = data.aws_vpc.VPC.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = "${var.app_port}"
+    to_port         = "${var.app_port}"
+    security_groups = ["${aws_security_group.lb.id}"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+### ALB
+
+resource "aws_alb" "main" {
+  name            = "tf-ecs-chat"
+  subnets         = data.aws_subnet_ids.public_subnets.ids
+  security_groups = ["${aws_security_group.lb.id}"]
+}
+
+resource "aws_alb_target_group" "app" {
+  name        = "tf-ecs-chat"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.VPC.id
+  target_type = "ip"
+}
+
+# Redirect all traffic from the ALB to the target group
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.app.id}"
+    type             = "forward"
+  }
+}
+
+### ECS
+
+resource "aws_ecs_cluster" "main" {
+  name = "tf-ecs-cluster"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "${var.fargate_cpu}"
+  memory                   = "${var.fargate_memory}"
+  execution_role_arn = aws_iam_role.ecs-role.arn # this is the role for ecs to pull images
+  task_role_arn = aws_iam_role.ecs-role.arn
+  container_definitions = <<DEFINITION
+[
+  {
+    "cpu": ${var.fargate_cpu},
+    "image": "${var.app_image}",
+    "memory": ${var.fargate_memory},
+    "name": "app",
+    "networkMode": "awsvpc",
+    "portMappings": [
+      {
+        "containerPort": ${var.app_port},
+        "hostPort": ${var.app_port}
+      }
+    ]
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "tf-ecs-service"
+  cluster         = "${aws_ecs_cluster.main.id}"
+  task_definition = "${aws_ecs_task_definition.app.arn}"
+  desired_count   = "${var.app_count}"
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups = ["${aws_security_group.ecs_tasks.id}"]
+    subnets         = data.aws_subnet_ids.public_subnets.ids
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.app.id}"
+    container_name   = "app"
+    container_port   = "${var.app_port}"
+  }
+
+  depends_on = [
+    "aws_alb_listener.front_end",
+  ]
+}
