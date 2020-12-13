f1c8fc2ae8b89db2748097bff9055670b686c5b4
Josh Jaffe <josjaf@amazon.com>
2020-12-12 20:29:36 -0500

    adding task definition name

--- application/ecs.tf
+++ application/ecs.tf
@@ -149,6 +149,17 @@ resource "aws_ecs_task_definition" "app" {
 DEFINITION
 }
 
+resource "aws_ssm_parameter" "taskdefinition" {
+  name = "/${var.namespace}/ecs/taskdefinition"
+  type = "String"
+  value = aws_ecs_task_definition.app.id
+  tags = {
+    Name = var.namespace
+    environment = var.namespace
+  }
+}
+
+
 resource "aws_ecs_service" "main" {
   name            = var.ecs_service_name
   cluster         = "${aws_ecs_cluster.main.id}"
