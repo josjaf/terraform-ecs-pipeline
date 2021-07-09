b0e868399f1814643c81755ce31b2f8d1ce19e5c
Josh Jaffe <josjaf@gmail.com>
2021-07-09 13:32:37 -0400

    splitting out

---
+++ application/sg.tf
@@ -0,0 +1,45 @@
+
+### Security
+
+# ALB Security group
+# This is the group you need to edit if you want to restrict access to your application
+resource "aws_security_group" "lb" {
+  name        = "tf-ecs-alb"
+  description = "controls access to the ALB"
+  vpc_id      = data.aws_vpc.VPC.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Traffic to the ECS Cluster should only come from the ALB
+resource "aws_security_group" "ecs_tasks" {
+  name        = "tf-ecs-tasks"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = data.aws_vpc.VPC.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.app_port
+    to_port         = var.app_port
+    security_groups = ["${aws_security_group.lb.id}"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
